9206bc9fae940a0bb6ed361ff705b6e50e629c2c
ErgimanGU <ergisenja@hotmail.com>
2021-11-22 14:01:15 +0100

    fixes

---
+++ src/main/java/pleasefivebank/EntryPage/EntryPageController.java
@@ -0,0 +1,25 @@
+package pleasefivebank.EntryPage;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+
+public class EntryPageController {
+
+    @FXML
+    private Button LoginButton;
+
+    @FXML
+    private PasswordField LoginPassword;
+
+    @FXML
+    private TextField LoginUsername;
+
+    @FXML
+    void LoginButtonPressed(ActionEvent event) {
+
+    }
+
+}
