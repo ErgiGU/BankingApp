c83e37d81edf3a624cc2bc79596fdc97cc484a84
ErgimanGU <ergisenja@hotmail.com>
2021-11-24 18:41:54 +0100

    forgot password gui(fxml)

---
+++ src/main/java/pleasefivebank/Main.java
@@ -0,0 +1,49 @@
+package pleasefivebank;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.Parent;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+import pleasefivebank.Menus.EntryPage;
+
+public class Main extends Application {
+    //@Override
+    //public void start(Stage stage) throws IOException {
+        //FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("Entry-Page.fxml"));
+        //Scene scene = new Scene(fxmlLoader.load(), 765, 588);
+        //stage.setTitle("P5B");
+        //stage.setResizable(false);
+        //stage.setScene(scene);
+        //stage.show();
+    //}
+
+    @Override
+    public void start(Stage stage) {
+        try {
+
+            Parent root = FXMLLoader.load(getClass().getResource("Entry-Page.fxml"));
+            Scene scene = new Scene(root);
+            stage.setTitle("P5B");
+            stage.setResizable(false);
+            stage.setScene(scene);
+            stage.show();
+
+        } catch(Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) {
+        try {
+            new Mongo();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        EntryPage entrypage = new EntryPage();
+        EntryPage.createUser();
+        launch();
+    }
+}
