af2a998069195a3cfe2bd6d43b47553d7f57ecdd
JuanDoesCoding <juandz2000@gmail.com>
2021-12-06 17:27:01 +0100

    Added entrypage methods

--- src/main/java/pleasefivebank/EntryPage/EntryPage.java
+++ src/main/java/pleasefivebank/EntryPage/EntryPage.java
@@ -1,8 +1,44 @@
 package pleasefivebank.EntryPage;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+
 public class EntryPage {
-    public boolean login(String username, String password){
-        //logic to encripted here
+    public int login(String username, String password){
+        //encript
+
+        //check for encripted user with same credentials
+        String encriptedName = encript(username);
+        String encriptedPassword = encript(password);
+
+        if (userVerified(encriptedName,encriptedPassword)){
+            //key will be the user key to access his info in the database
+            int key = 1;
+            return key;
+
+        }
+        return -1;
+    }
+    public boolean register(HashMap userInfo){
+        //we first see if user exists
+
+        //if he doesnt we create account
+
+
         return false;
+    }
+    public String encript(String string){
+        //logic to encrypt here
+
+        //return encripted string
+        return string;
+    }
+    public boolean userVerified(String encriptedUsername, String encriptedPassword){
+        //if user is found and password corresponds
+
+        return true;
+
+        //else return false;
+
     }
 }
